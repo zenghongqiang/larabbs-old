3102c58165d6f0052410b42f84c15d30f389606b
e <zenghongqiang44@163.com>
2019-07-11 22:13:49 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 Route::resource('users','UsersController',['only' => ['show','update','edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
