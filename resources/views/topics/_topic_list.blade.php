96055b9ec7168b0b55a4d8607338a7185ce0693b
e <zenghongqiang44@163.com>
2019-07-11 23:16:43 +0800

    分类话题列表

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -21,15 +21,9 @@
 
           <small class="media-body meta text-secondary">
 
-            <a class="text-secondary" href="#" title="{{ $topic->category->name }}">
+            <a class="text-secondary" href="{{ route('categories.show', $topic->category_id) }}" title="{{ $topic->category->name }}">
               <i class="far fa-folder"></i>
               {{ $topic->category->name }}
-            </a>
-
-            <span> • </span>
-            <a class="text-secondary" href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}">
-              <i class="far fa-user"></i>
-              {{ $topic->user->name }}
             </a>
             <span> • </span>
             <i class="far fa-clock"></i>
