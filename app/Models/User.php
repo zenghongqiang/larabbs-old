3ccb1c44d2e61bf67a157b51ac6ed85954f1ecdf
HQzeng <zenghongqiang44@163.com>
2019-09-19 18:27:25 +0800

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->id == $model->user_id;
     }
+     public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
